ff8d63a3a5daff15072aa585782c9377f44c2167
diegoteis1222 <diegoteis122@gmail.com>
2025-03-11 11:54:25 +0100

    Añadido el ejercicio de Discos funcionando

---
+++ Programación/EjemplosEjercicios/Discos/Diego.java
@@ -0,0 +1,74 @@
+package GitArchivos.Programación.EjemplosEjercicios.Discos;
+
+import java.io.Serializable;
+
+public class Diego implements Serializable {
+
+    private String codigo = "LIBRE";
+    private String autor;
+    private String titulo;
+    private String genero;
+    private int duracion;
+
+    public Diego(String codigo, String autor, String titulo, String genero, int duracion) {
+        super();
+        this.codigo = codigo;
+        this.autor = autor;
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracion = duracion;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    @Override
+    public String toString() {
+        String cadena="\n-----------------------------------";
+        cadena+="\nCodigo: "+this.codigo;
+        cadena+="\nAutor: "+this.autor;
+        cadena+="\nTitulo: "+this.titulo;
+        cadena+="\nGenero: "+this.genero;
+        cadena+="\nDuracion: "+this.duracion;
+        cadena+="\n-----------------------------------------";
+        return cadena;
+        }
+
+}
---
+++ Programación/EjemplosEjercicios/Discos/testDiego.java
@@ -0,0 +1,238 @@
+package GitArchivos.Programación.EjemplosEjercicios.Discos;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class testDiego implements Serializable {
+
+    static ArrayList<Diego> discos;
+    static Scanner sc = new Scanner(System.in);
+
+    public static void crearColeccion() {
+        discos = new ArrayList<Diego>();
+    }
+
+    // mock de datos
+    public static void iniciaColeccion() {
+        discos.add(new Diego("GASA41", "Wim Mertens", "Maximazing the Audience", "instrumental", 50));
+        discos.add(new Diego("FGHQ64", "Metallica", "Black album", "hard rock", 46));
+        discos.add(new Diego("TYUI89", "Supersubmarina", "Viento de cara", "pop rock", 42));
+    }
+
+    public static void mostrarColeccion() {
+        for (Diego d : discos) {
+            if (d != null) {
+                System.out.println(d.toString());
+            }
+        }
+    }
+
+    private static void pideDisco() {
+
+        System.out.println("Introduzca los datos del disco: ");
+        sc.nextLine();
+
+		System.out.print("Código: ");
+		String codigoIn = sc.nextLine();
+
+        // habria q ver si no repetimos codigo
+        System.out.print("Autor: ");
+        String autorIn = sc.nextLine();
+
+        System.out.print("Título: ");
+        String tituloIn = sc.nextLine();
+
+        System.out.print("Género: ");
+        String generoIn = sc.nextLine();
+
+        System.out.print("Duración: ");
+        int duracionIn = Integer.parseInt(sc.nextLine());
+
+        System.out.println("Seguro que quieres crear este disco?\ns/n");
+        int res = sc.nextLine().charAt(0);
+        if (res == 's') {
+            discos.add(new Diego(codigoIn, autorIn, tituloIn, generoIn, duracionIn));
+            System.out.println("DISCO CREADO CON EXITO...");
+        }
+    }
+
+    public static void borrarDisco() {
+
+        boolean esta = false;
+        System.out.println("Introduce Codigo de disco para borrar");
+        sc.nextLine();
+        System.out.println("Codigo:");
+        String codigoIn = sc.nextLine();
+
+        // metodo busqueda
+        for (Diego dis : discos) {
+            if (dis.getCodigo().equals(codigoIn)) {
+                System.out.println("Disco encontrado...");
+                System.out.println(dis.toString());
+                esta = true;
+            }
+            if (esta == true) {
+                System.out.println("Quieres borrar ese disco? 1:SI 2:NO");
+                int decision = sc.nextInt();
+
+                if (decision == 1) {
+                    if (dis != null) {
+                        discos.remove(dis);
+                        System.out.println("Disco borrao...");
+                        return;
+                    }
+                }
+            }
+            // fin decision
+        } // fin for
+        return;
+    }
+
+    public static void modificarDisco() {
+
+        boolean esta = false;
+        System.out.println("Introduce Codigo de disco para borrar");
+        sc.nextLine();
+        System.out.println("Codigo:");
+        String codigoIn = sc.nextLine();
+
+        // metodo busqueda
+        for (Diego dis : discos) {
+            if (dis.getCodigo().equals(codigoIn)) {
+                System.out.println("DISCO ENCONTRADO...");
+                System.out.println(dis.toString());
+                esta = true;
+            }
+            if(esta==false) {
+                System.out.println("DISCO NO ENCONTRADO...");
+            }
+            if (esta == true) {
+
+                System.out.println("Seguro que quieres modificar este disco?\ns/n");
+                int res = sc.nextLine().charAt(0);
+                if (res == 's') {
+                    System.out.println("Por favor, introduzca los nuevos datos del disco.");
+                    System.out.print("Código: ");
+                    dis.setCodigo(sc.nextLine());
+                    // habria q ver si no repetimos codigo
+                    System.out.print("Autor: ");
+                    dis.setAutor(sc.nextLine());
+                    System.out.print("Título: ");
+                    dis.setTitulo(sc.nextLine());
+                    ;
+                    System.out.print("Género: ");
+                    dis.setGenero(sc.nextLine());
+                    System.out.print("Duración: ");
+                    dis.setDuracion(Integer.parseInt(sc.nextLine()));
+                    System.out.println("\nDISCO MODIFICADO CON EXITO...");
+                }
+
+                return;
+
+            }
+        }
+    }
+
+    private static void guardarColeccion() {
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("misDiscos.bin"))) {
+
+            oos.writeObject(discos);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void cargarColeccion() throws ClassNotFoundException  {
+        //recuperar array list de
+        try (ObjectInputStream ois= new ObjectInputStream(new FileInputStream("misDiscos.bin"))){
+            discos=(ArrayList<Diego>) ois.readObject();
+            System.out.println("COleeccion de discos recuperada");
+            for(Diego elem:discos) {
+                System.out.println(elem);
+            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        crearColeccion();
+        iniciaColeccion();
+        mostrarColeccion();
+
+        int opcion = 0;
+
+        do {
+            System.out.println("\n Colecion de discos");
+            System.out.println("====================================");
+            System.out.println("1. Mostrar coleccion");
+            System.out.println("2. Nuevo disco");
+            System.out.println("3. Modificar disco");
+            System.out.println("4. Borrar disco");
+            System.out.println("5. Guardar coleccion");
+            System.out.println("6. Cargar coleccion");
+            System.out.println("7. Salir\n");
+
+            System.out.println("Introduzca opcion: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Mostrar coleccion");
+                    System.out.println("====================");
+
+                    mostrarColeccion();
+
+                    break;
+                case 2:
+                    System.out.println("Nuevo disco");
+                    System.out.println("====================");
+
+                    pideDisco();
+
+                    break;
+                case 3:
+                    System.out.println("Modificar disco");
+                    System.out.println("====================");
+
+                    modificarDisco();
+
+                    break;
+                case 4:
+                    System.out.println("Borrar disco");
+                    System.out.println("====================");
+
+                    borrarDisco();
+
+                    break;
+                case 5:
+                    System.out.println("guardar coleccion");
+                    System.out.println("====================");
+
+                    guardarColeccion();
+
+                    break;
+                case 6:
+                    System.out.println("Cargar coleccion");
+                    System.out.println("====================");
+
+                    cargarColeccion();
+                    break;
+                case 7:
+                    System.out.println("Hasta luego");
+                    sc.close();
+                    break;
+                default:
+                    System.out.println("Opcion incorrecta");
+            }
+        } while (opcion != 7);
+
+    }
+}
