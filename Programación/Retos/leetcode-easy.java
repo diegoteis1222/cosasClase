90672d4d6f80f862f7e5592eb42e731499eec06a
diegoteis122 <diegoteis122@gmail.com>
2025-04-12 19:55:48 +0200

    añadido removeDuplicates

--- Programación/Retos/leetcode-easy.java
+++ Programación/Retos/leetcode-easy.java
@@ -1,6 +1,5 @@
 package Retos;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 class Solution {
@@ -127,6 +126,36 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
 
             if (nums[i] != val) {
+                nums[cont] = nums[i];
+                cont++;
+            }
+        }
+        return cont;
+    }
+
+    /**
+     * Given an integer array nums sorted in non-decreasing order, remove the
+     * duplicates in-place such that each unique element appears only once. The
+     * relative order of the elements should be kept the same. Then return the
+     * number of unique elements in nums.
+     * 
+     * Consider the number of unique elements of nums to be k, to get accepted, you
+     * need to do the following things:
+     * 
+     * Change the array nums such that the first k elements of nums contain the
+     * unique elements in the order they were present in nums initially. The
+     * remaining elements of nums are not important as well as the size of nums.
+     * Return k.
+     * 
+     * @param nums array de numeros dado
+     * @return la cantidad de elementos unicos, elimina los duplicados de un array
+     * @see https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+     */
+    public static int removeDuplicates(int[] nums) {
+        int cont = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[cont - 1]) {
                 nums[cont] = nums[i];
                 cont++;
             }
