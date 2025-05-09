f042f457b6c66e04ddc63b6fc1b3ba1c4d9aeb22
diegoteis1222 <diegoteis122@gmail.com>
2025-05-09 12:56:25 +0200

    terminado el test de las notas

--- Programación/SW2/Test.java
+++ Programación/SW2/Test.java
@@ -1,5 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
     }
 }
