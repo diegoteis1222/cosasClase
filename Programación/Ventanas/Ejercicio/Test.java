b3d20f8e83d53327a890edce8fa9023cd78e32d5
diegoteis122 <diegoteis122@gmail.com>
2025-05-11 02:03:21 +0200

    Echo el ejercicio de programacion

---
+++ Programación/Ventanas/Ejercicio/Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+
+        // Joder, porque le puedo preguntar a gepeto y copie buena parte del ejercicio de personas 
+        // que yo esto no lo sacaba ni pagandome
+
+        // almenos la colocacion de los items en pantlla quedó bien chulardo
+    }
+}
